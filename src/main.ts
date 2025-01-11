cdc95d3f3417bde85372b937d0afd89d904a872d
김민형 <alsgud940713@gmail.com>
2025-01-12 00:49:31 +0900

    feat: main.ts 수정 -> Vuetify 추가

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,13 @@
-// src/main.ts
 import { createApp } from 'vue';
 import App from './App.vue';
 import { createPinia } from 'pinia';
+import { vuetify } from './plugins/vuetify'; // Vuetify 설정을 분리한 파일 import
 
-// Pinia 추가
 const app = createApp(App);
+
+// Pinia 및 Vuetify 추가
 app.use(createPinia());
+app.use(vuetify);
 
 // Vue 애플리케이션 시작
 app.mount('#app');
