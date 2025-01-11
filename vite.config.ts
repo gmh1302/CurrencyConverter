d46ac3444935ae3f7846ceeb1ba63f4455bd3db5
김민형 <alsgud940713@gmail.com>
2025-01-12 00:48:37 +0900

    feat: vuetify 설절 추가

--- vite.config.ts
+++ vite.config.ts
@@ -2,14 +2,19 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import vuetify from '@vuetify/vite-plugin';
 
 export default defineConfig({
     plugins: [
-        vue(),
+        vue(), vuetify({ autoImport: true })
     ],
     resolve: {
         alias: {
             '@': fileURLToPath(new URL('./src', import.meta.url))
         }
-    }
+    },
+    // server: {
+    //   host: '0.0.0.0', // 모든 네트워크 인터페이스에서 접근 허용
+    //   port: 3000, // 원하는 포트 설정
+    // },
 })
