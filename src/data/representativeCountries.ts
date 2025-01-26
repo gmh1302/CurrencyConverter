2c33b539eca5f6857d83e2360dd90a3e4fa5ba59
김민형 <alsgud940713@gmail.com>
2025-01-27 01:35:44 +0900

    feat: 국가 데이터 파일 생성

---
+++ src/data/representativeCountries.ts
@@ -0,0 +1,27 @@
+export const representativeCountries = {
+    KRW: "South Korea",        // 대한민국
+    ZAR: "South Africa",       // 남아프리카 공화국
+    NOK: "Norway",             // 노르웨이
+    NZD: "New Zealand",        // 뉴질랜드
+    DKK: "Denmark",            // 덴마크
+    BRL: "Brazil",             // 브라질
+    SEK: "Sweden",             // 스웨덴
+    CHF: "Switzerland",        // 스위스
+    SGD: "Singapore",          // 싱가포르
+    GBP: "United Kingdom",     // 영국
+    INR: "India",              // 인도
+    IDR: "Indonesia",          // 인도네시아
+    JPY: "Japan",              // 일본
+    CNY: "China",              // 중국
+    CZK: "Czechia",            // 체코
+    MXN: "Mexico",             // 멕시코
+    MYR: "Malaysia",           // 말레이시아
+    PHP: "Philippines",        // 필리핀
+    PLN: "Poland",             // 폴란드
+    HUF: "Hungary",            // 헝가리
+    AUD: "Australia",          // 호주
+    HKD: "Hong Kong",          // 홍콩
+    TRY: "Turkey",             // 튀르키예
+    THB: "Thailand",           // 태국
+    USD: "United States"       // 미국
+};
